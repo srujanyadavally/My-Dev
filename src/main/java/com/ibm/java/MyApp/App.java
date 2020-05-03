4d0d7bf63c8ec02e4532c7223937555a1d61a911
srujanyadavalli <srujan.yadavalli@outlook.com>
2020-05-03 20:13:02 +0530

    new feature Product

--- src/main/java/com/ibm/java/MyApp/App.java
+++ src/main/java/com/ibm/java/MyApp/App.java
@@ -10,16 +10,17 @@ public class App
     public static void main( String[] args )
     {
         Scanner var1 = new Scanner(System.in);
-        System.out.println("Enter Number");
+        System.out.println("Enter number");
         int num1 = var1.nextInt();
         
         Scanner var2 = new Scanner(System.in);
-        System.out.println("Enter Number");
-        int num2 = var2.nextInt();
+        System.out.println("Enter number");
+        int num2 = var2.nextInt();        
         
-        int result = num1 + num2;
         
-        System.out.println("The sum of two numbers is:"+ result);
-        
+        int Sum = num1+num2;
+        int Product = num1*num2;
+        System.out.println("The sum of  two numbers is:"+ Sum);
+        System.out.println("The product of  two numbers is:"+ Product);
     }
 }
