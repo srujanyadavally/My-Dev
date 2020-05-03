53d3204a3775443933d76ecce5e4a09452637454
srujanyadavalli <srujan.yadavalli@outlook.com>
2020-05-03 18:26:31 +0530

    Sum function

--- src/main/java/com/ibm/java/MyApp/App.java
+++ src/main/java/com/ibm/java/MyApp/App.java
@@ -1,4 +1,5 @@
 package com.ibm.java.MyApp;
+import java.util.Scanner;
 
 /**
  * Hello world!
@@ -8,6 +9,17 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Scanner var1 = new Scanner(System.in);
+        System.out.println("Enter Number");
+        int num1 = var1.nextInt();
+        
+        Scanner var2 = new Scanner(System.in);
+        System.out.println("Enter Number");
+        int num2 = var2.nextInt();
+        
+        int result = num1 + num2;
+        
+        System.out.println("The sum of two numbers is:"+ result);
+        
     }
 }
